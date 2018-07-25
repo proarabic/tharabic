323329f087844abd5c88714541c04f02f0a9e5fb
proarabic <30710501+proarabic@users.noreply.github.com>
2018-07-25 12:10:50 +0100

    Update and rename cop.js to th1.js

---
+++ th1.js
@@ -0,0 +1,1 @@
+$(document)['ready'](function(){$('#dontedit')['html']('<a href="https://www.abdoutech.com" rel="dofollow" target="_blank" style="visibility:visible!important;opacity:1!important;position:relative!important">*5EJE:9(/H *CFHDH,J</a>'),setInterval(function(){$('#dontedit:visible')['length']||(window['location']['href']='https://www.tharabic.com')},3e3)});
